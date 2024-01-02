899064451f41e84ccdf09777c16ab466b67b2a4a
SMun9 <osmun91@gmail.com>
2024-01-03 00:32:13 +0900

    modify models.py

--- miniter/models.py
+++ miniter/models.py
@@ -4,7 +4,7 @@ import datetime
 
 
 class Users(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, )
     name = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), nullable=False)
     hashed_password = db.Column(db.String(255), nullable=False)
@@ -17,12 +17,14 @@ class UsersFollowList(db.Model):
     follow_user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='CASCADE'), primary_key=True)
     create_at = db.Column(db.DateTime(), nullable=False, default=datetime.datetime.utcnow)
     # user = db.relationship('Users', backref=db.backref('UsersFollowList', lazy=True))
+    
 
 class Tweets(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
     tweet = db.Column(db.String(300), nullable=False)
     create_at = db.Column(db.DateTime(), nullable=False, default=datetime.datetime.utcnow)
+    users = db.relationship("Users", backref=db.backref("user_set"))
     
     
 # Unique constraint on the email column in the User table
